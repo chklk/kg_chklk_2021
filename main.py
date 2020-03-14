42aecd4f55e6a4b6c4913c188f00d521e25e79ef
Kelly Liu <jiayiliu@jiayis-MacBook-Pro.local>
2020-03-14 16:50:22 -0400

    Add main.py

---
+++ main.py
@@ -0,0 +1,19 @@
+import os
+import sys
+
+def onetoone(s1, s2): 
+	if len(s1)!=len(s2): 
+		return False
+	word_dict={}
+	for i in range(len(s1)): 
+		if s1[i] in word_dict: 
+			if word_dict[s1[i]]!=s2[i]: 
+				return False 
+		else: 
+			word_dict[s1[i]]=s2[i]
+	return True
+
+if __name__ == '__main__':
+	s1=str(sys.argv[1])
+	s2=str(sys.argv[2])
+	print(onetoone(s1, s2))
